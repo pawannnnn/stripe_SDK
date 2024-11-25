9140a9187912dcbde049592caed54a2bc864b33c
pawannnnn <150579399+pawannnnn@users.noreply.github.com>
2024-11-25 07:24:08 +0530

    manage customers with methods for creating retrieving customer details

--- src/api/customers.js
+++ src/api/customers.js
@@ -1,1 +1,29 @@
+const stripe = require('../config/stripeConfig')
+async function createCustomer(data){
+  try{
+    const customer = await stripe.customers.create(data)
+    
+    return customer 
+    
+  }
+  catch(error){
+    console.log(`${error.message}`)
+    
+    
+  }
+}
 
+async function retrieveCustomer(customerId){
+  try{
+    const retrieved = await stripe.customers.retrieve(customerId)
+    return retrieved 
+  }
+  catch(error){
+    console.log(`$error.message}`)
+  }
+}
+module.exports={
+  retrieveCustomer,
+  createCustomer
+  
+}
