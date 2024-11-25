83b19143ba631c38de946fc151a55b95a3260e89
pawannnnn <150579399+pawannnnn@users.noreply.github.com>
2024-11-25 08:11:58 +0530

    This file handles subscriptions with functions for creating , deleting subscriptions

--- src/api/subscriptions.js
+++ src/api/subscriptions.js
@@ -1,1 +1,33 @@
+const stripe= require('../config/stripeConfig')
 
+
+async function createSubscription(customerId,priceId,options={}){
+  try{
+    const subscription = await stripe.subscriptions.create({
+      customer: customerId,
+      items:[{price: priceId}]
+      ...options ,
+                                                           
+      
+    })
+    
+    return subscription 
+    
+  }
+    
+  catch(error){
+
+    console.log(`${error.message}`)
+  }
+  
+}
+
+async function cancelSubscription(subscriptionId){
+  try{
+    const cancelledSubscription =await stripe.subscriptions.del(subscriptionId)
+    return cancelledSubscription}
+  catch(error){
+    throw new Error(`${error.message}`)
+  }
+}
+  
