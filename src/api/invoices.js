80f44dc51b78ac9a6c4ce724031bef25b7c953fc
pawannnnn <150579399+pawannnnn@users.noreply.github.com>
2024-11-25 08:28:38 +0530

    This file includes handling creating invoices and paying invoices

--- src/api/invoices.js
+++ src/api/invoices.js
@@ -1,1 +1,35 @@
 
+const stripe = require('...config/stripeConfig')
+async function createInvoice(customerId,items){
+  try{
+    const invoicePromises= items.map((item)=>{
+     stripe.invoiceItems.create({
+       customer:customerId, amount :amount:item.amount
+       , currency:item.currency, 
+       description: item.description 
+       
+     })
+    })
+    await Promise.all(invoicePromises)
+    
+  }
+  catch(error){
+    throw new Error(`${error.message}`)
+  }
+  
+  
+}
+
+
+async function payInvoice(invoiceId){
+  try{
+    const payedVoice= await stripe.invoices.pay(invoiceId)
+    return payedVoice 
+    
+  }
+  catch(error){
+    thorw new error(`${error.nessage}`)
+  }
+  
+}
+
