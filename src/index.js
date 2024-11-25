7219fe36a0ce578e958ba7b544eabd3fadf5a362
pawannnnn <150579399+pawannnnn@users.noreply.github.com>
2024-11-25 08:40:31 +0530

    root point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-
+const customers = require('../api/customer')
+const invoices= require('../api/invoice')
+const payments = require('../api/payments')
+const subscriptions= require('../app/subscriptions')
+module.exports={
+  customers, invoices, payments ,subscriptions
+  }
