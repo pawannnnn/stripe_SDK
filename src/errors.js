a2af5a3bdc05e53109eeba6b69e3de5eff76cef1
pawannnnn <150579399+pawannnnn@users.noreply.github.com>
2024-11-25 08:35:37 +0530

    this file is extended the custom errors to show off the stripe errors

--- src/errors.js
+++ src/errors.js
@@ -1,1 +1,14 @@
+class StripeError extends Error{
+  constructor(message){
+    super(message){
+      this.message= ' this is an error'
+      
+    } 
+  }
+}
 
+
+module.exports={
+
+  StripeError
+}
