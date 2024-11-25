663a362534f6768f4952355f6d8a361c2ccf3b1f
pawannnnn <150579399+pawannnnn@users.noreply.github.com>
2024-11-25 07:16:13 +0530

    this file handles payment processing and capturing payments btw

--- src/api/payments.js
+++ src/api/payments.js
@@ -1,1 +1,37 @@
-// contains payments processing methos 
+// contains payments processing methods
+
+
+const stripe= require('../config/stripeConfig')
+
+
+// here i create  payment intent 
+
+
+async function createPaymentIntent(amount,currency,options={}){
+  
+try{
+  const paymentIntent= await stripe.paymentIntents.create({
+
+    amount,currency,...options 
+  })
+  return paymentIntent
+  
+}catch(error){
+  console.log(`failed to create paymenr ${error.message}`)
+  
+}
+  
+
+  // the code here  captire a payment or methods 
+  async function capturePaymentIntent(paymentId){
+    try{
+
+      const capturedPayment= await stripe.paymentIntents.capture(paymentId)
+      return capturePayment 
+    }
+    
+  catch(error){
+    console.log(`${error.message}`)
+  }
+ 
+}
