a4253845d29772c5a84605856f49535f48a43f22
pawannnnn <150579399+pawannnnn@users.noreply.github.com>
2024-11-25 08:44:03 +0530

    This file consists of mocha and unit tests for testing payment modules

--- examples/subscription-setup.js
+++ examples/subscription-setup.js
@@ -1,1 +1,14 @@
+// test/payments.test.js
+const { payments } = require('../src');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Payments API', () => {
+  it('should create a payment intent', async () => {
+    const paymentIntent = await payments.createPaymentIntent(1000, 'usd', { description: 'Test Payment' });
+    expect(paymentIntent).to.have.property('id');
+    expect(paymentIntent.amount).to.equal(1000);
+    expect(paymentIntent.currency).to.equal('usd');
+  });
+});
 
